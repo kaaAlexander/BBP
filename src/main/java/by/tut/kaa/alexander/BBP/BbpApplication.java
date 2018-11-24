ff6fc4e51a0a1d12e7e835afbbf2b89b8bc9b656
cops2002@mail.ru <fuckyouspielberg>
2018-11-24 13:36:19 +0300

    Start

---
+++ src/main/java/by/tut/kaa/alexander/BBP/BbpApplication.java
@@ -0,0 +1,12 @@
+package by.tut.kaa.alexander.BBP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbpApplication.class, args);
+	}
+}
---
+++ src/test/java/by/tut/kaa/alexander/BBP/BbpApplicationTests.java
@@ -0,0 +1,16 @@
+package by.tut.kaa.alexander.BBP;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BbpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
